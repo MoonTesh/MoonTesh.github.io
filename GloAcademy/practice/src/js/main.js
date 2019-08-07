c7a86abf960a310ad40150b2251320f60b517900
MoonTesh <n_sirenko2004@ukr.net>
2019-08-07 14:08:46 +0300

    practice11
    Добавлена и адаптирована секция с материалами

--- GloAcademy/practice/src/js/main.js
+++ GloAcademy/practice/src/js/main.js
@@ -57,6 +57,35 @@
             ]
         });
 
+        /* ------ Materials Slider------ */
+
+        $('.materials-slider').slick({
+            infinite: true,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            responsive: [
+               {
+                 breakpoint: 992,
+                 settings: {
+                   slidesToShow: 2,
+                   prevArrow: '<button class="prev arrow"></button>',
+                   nextArrow: '<button class="next arrow"></button>',
+                   slidesToScroll: 2,
+                   infinite: true,
+                 }
+                },
+               {
+                 breakpoint: 768,
+                 settings: {
+                   slidesToShow: 1,
+                   slidesToScroll: 1,
+                   prevArrow: '<button class="prev arrow"></button>',
+                   nextArrow: '<button class="next arrow"></button>'
+                  }
+                } 
+            ]
+      });
+
        /* ------ Modal Windows------ */ 
        $('.popup-btn').on('click', function(event){
             event.preventDefault();
